2317665e869604694761e399c7eb09106d9f33b3
Kersauson <colin.kerouanton@gmail.com>
2017-03-29 12:24:59 +0200

    Update functions.r

--- functions.r
+++ functions.r
@@ -25,28 +25,34 @@ realdistR = function(p1, p2, reso, mnt_grid, profil_out = FALSE){
   lat = c(p1[,"y"], p2[,"y"])
   
   d = dist(cbind(lon, lat))
+  
   opt = d/reso
   
-  ns = spline(lon, lat, opt)
+  if (d > reso){
+    ns = as.data.frame(spline(lon, lat, opt))
+  } else {
+    ns = data.frame(x = lon, y = lat)
+  }
   
-  points = data.frame(x = ns$x, y = ns$y)
-  points = alt_ext(spatial_traj93(points, points$x, points$y), mnt_grid)
   
-  points$dist = 0
-  points$distr = 0
-  points$dcum = 0
   
-  for (i in 2:nrow(points)){
-    points$dist[i] = dist(rbind(c(points$x[i], points$y[i]), c(points$x[i-1], points$y[i-1])))
-    h = points$alti[i] - points$alti[i-1]
-    points$distr[i] = sqrt(points$dist[i]^2 + h^2)
-    points$dcum[i] = points$dcum[i-1] + points$dist[i]
+  ns = alt_ext(spatial_traj93(ns, ns$x, ns$y), mnt_grid)
+  
+  ns$dists = 0
+  ns$distr = 0
+  ns$dcum = 0
+  
+  for (i in 2:nrow(ns)){
+    ns$dists[i] = dist(rbind(c(ns$x[i], ns$y[i]), c(ns$x[i-1], ns$y[i-1])))
+    h = ns$alti[i] - ns$alti[i-1]
+    ns$distr[i] = sqrt(ns$dists[i]^2 + h^2)
+    ns$dcum[i] = ns$dcum[i-1] + ns$dists[i]
   }
   
   if(profil_out){
-    exe = points[points$alti > 0,]
-    plot(x = exe$dcum, y = exe$alti, type = "l", lwd = 2)
+  exe = ns[ns$alti > 0,]
+  plot(x = ns$dcum, y = ns$altis, type = "l", lwd = 2)
   }
-  return(sum(points$distr))
+  return(sum(ns$distr))
 }
 
